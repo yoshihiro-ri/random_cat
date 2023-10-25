4260a0a988160a87ee271935c891560638acadc0
yoshihiro-ri <ri.dev1027@gmail.com>
2023-10-25 15:12:53 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { NextPage } from "next";
+import { useEffect,useState} from "react"
+const IndexPage: NextPage = () => {
+    const [imageUrl, setImageUrl] = useState("")
+    const [loading,setLoading] = useState(true)
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+    useEffect(()=>{
+        fetchImage().then((newImage) => {
+            setImageUrl(newImage.url);
+            setLoading(false);
+        });
+    },[]);
 
-export default IndexPage
+    return <div>{loading || <img src={imageUrl}/>}</div>;
+};
+
+export default IndexPage;
+type Image = {
+    url: string;
+}
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    return images[0];
+};
